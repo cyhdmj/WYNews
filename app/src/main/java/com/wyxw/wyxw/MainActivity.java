f1d61b2d6cb630740a48fd1a10c40e3dd22e68cc
zhangziyang <3057033822@qq.com>
2016-09-28 14:40:44 +0800

    init commit

---
+++ app/src/main/java/com/wyxw/wyxw/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wyxw.wyxw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
